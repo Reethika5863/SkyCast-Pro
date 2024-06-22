7fdbba20b2cdadae8a60a80aec75363001dce366
M.Reethika <reethu3361@gmail.com>
2024-06-22 14:13:04 +0530

    SKYPRO

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import contact
 
 def index(request):
     return render(request,"index.html")
@@ -7,5 +8,16 @@ def index(request):
 def about(request):
     return render(request,"about.html")
 
-def contact(request):
+def cont(request):
     return render(request,"contact.html")
+
+def docontact(request):
+    mobile=request.GET['mobile']
+    email=request.GET['email']
+    address=request.GET['address']
+    c=contact()
+    c.Mobile=mobile
+    c.email=email
+    c.address=address
+    c.save()
+    return render(request,"contact.html",{"msg":"Contact successfull"})
